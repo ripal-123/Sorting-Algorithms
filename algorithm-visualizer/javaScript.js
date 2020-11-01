8129d0f01672c2cc003268697246b7f40519e705
Ripal Jindal <47396713+ripal-123@users.noreply.github.com>
2020-11-01 14:14:22 +0530

    Add files via upload

--- algorithm-visualizer/javaScript.js
+++ algorithm-visualizer/javaScript.js
@@ -1,18 +1,20 @@
 const bars = document.querySelector(".bars");
 const algorithm = document.querySelector(".algorithnm");
 const msort = document.querySelector("#mergeSort");
+const insort = document.querySelector("#insertionSort");
+const bsort = document.querySelector("#bubbleSort");
 const slider = document.querySelector(".slider");
 let array = [];
 let map = new Map();
 
 const styleBar = (element, height, width, w) =>{
     element.style.alignText =  "vertical-center";
-    //element.style.padding = "1px";
+    //element.style.padding = `${100/w}%`;
     element.style.height = `${height}px`;
-    element.style.width = `${(80/width - 100/w)}%`;
+    element.style.width = `${(80/width) - (100/w)}%`;
     element.style.display = "inline-block";
     element.style.backgroundColor = "blue";
-    element.style.margin = "1px";
+    element.style.margin = `0 ${100/w}%`;
 }
 
 const resetBars = (array, size) => {
@@ -20,13 +22,34 @@ const resetBars = (array, size) => {
     const width = window.screen.availWidth - 20;
     for(let i = 0;i<size;i++)
     {
-        array[i] = Math.floor((Math.random() * 100000)%490);
+        array[i] = Math.floor((Math.random() * 100000)%490+5);
         let element = document.createElement("p");
         map[i]=array[i];
         element.setAttribute("id", `id${i}`);
         styleBar(element, array[i], size, width);
         bars.appendChild(element)
     }
+}
+
+const delay = (seconds) => {
+    return new Promise(
+        resolve => setTimeout(resolve, seconds)
+    );
+};
+
+const doAnimation = async (item) => {
+    let bar1 = document.querySelector(`#id${item[0]}`);
+    let bar2 = document.querySelector(`#id${item[1]}`);
+    bar1.style.backgroundColor = "red";
+    bar2.style.backgroundColor = "red";
+    await delay(1);
+    let h1 = bar1.style.height;
+    let h2 = bar2.style.height;
+    bar1.style.height = h2;
+    bar2.style.height = h1;
+    await delay(1);
+    bar1.style.backgroundColor = "blue";
+    bar2.style.backgroundColor = "blue";
 }
 const merge = (arr1, arr2) => {
     let i=0;
@@ -35,21 +58,15 @@ const merge = (arr1, arr2) => {
     let temp = [];
     for(;i<arr1.length && j<arr2.length;k++) {
         if(arr1[i] < arr2[j]) {
-            temp[k] = (arr1[i]);
+            temp.push(arr1[i]);
             i++;
         } else {
-            temp[k] = (arr2[j]);
+            temp.push(arr2[j]);
             j++;
         }
     }
-    if(i<arr1.length) 
-    {
-        for(;i<arr1.length;) temp[k++] = (arr1[i++]);
-    }
-    if(j<arr2.length) 
-    {
-        for(;j<arr2.length;) temp[k++] = (arr2[j++]);
-    }
+    if(i<arr1.length) temp.push(...arr1.slice(i, arr1.length)); //(...) spread operator
+    if(j<arr2.length) temp.push(...arr2.slice(j, arr2.length));
     return temp;
 }
 
@@ -70,8 +87,39 @@ const mergeSort = (array, l, r) => {
     }
 }
 
+const insertionSort = async(array) => {
+    let animation = [];
+    for(let i=1;i<array.length;i++) {
+        for(let j=i;j>0;j--) {
+            if(array[j] < array[j-1]) {
+                let temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+                await doAnimation([j, j-1]);
+            } else {
+                break;
+            }
+        }
+    }
+    //doAnimation(animation);
+}
+const bubbleSort = async (array) => {
+    animation = [];
+    for(let i=0;i<array.length;i++) {
+        for(let j=0;j<array.length-1;j++) {
+            if(array[j] > array[j+1]) {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                await doAnimation([j, j+1]);
+            }
+        }
+    }
+}
 resetBars(array, slider.value);
 
 msort.addEventListener("click", function(){mergeSort(array, 0, slider.value - 1)});
+insort.addEventListener("click", function(){insertionSort(array)});
+bsort.addEventListener("click", function(){bubbleSort(array)});
 slider.addEventListener("input", function(){resetBars(array, slider.value)});
-console.log(array);
+console.log(array); 
